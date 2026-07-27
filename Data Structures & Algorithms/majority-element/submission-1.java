20b7bb4d25547e6eb3a4e76fede4603ea297aa73
Ayush Kumar <40884239+ayush487@users.noreply.github.com>
2026-07-27 23:14:30 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    // Boyer-Moore Voting Algorithm
+    public int majorityElement(int[] nums) {
+        int res = nums[0];
+        int count = 1;
+        for (int i=1;i<nums.length;i++) {
+            if (nums[i]==res) count++;
+            else {
+                if (count==0) {
+                    res = nums[i];
+                    count = 1;
+                } else count--;
+            }
+        }
+        return res;
+    }
+}
