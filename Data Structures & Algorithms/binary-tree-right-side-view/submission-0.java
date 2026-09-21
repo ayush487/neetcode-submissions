a5d86c833ea532f0a0be8a6665f6dfb35741a8d5
Ayush Kumar <40884239+ayush487@users.noreply.github.com>
2026-09-21 17:07:29 +0530

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    private static class TreeNodeLevels {
+        int level;
+        TreeNode node;
+
+        TreeNodeLevels(int level, TreeNode node) {
+            this.level = level;
+            this.node = node;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> levelOrderList = levelOrder(root);
+        for (List<Integer> level : levelOrderList) {
+            list.add(level.getLast());
+        }
+        return list;
+    }
+
+    private List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<TreeNodeLevels> queue = new LinkedList<>();
+        if (root != null) queue.offer(new TreeNodeLevels(0, root));
+        while (!queue.isEmpty()) {
+            TreeNodeLevels lNode = queue.poll();
+            int alSize = list.size();
+            if (alSize == lNode.level) {
+                list.add(new ArrayList<>());
+            }
+            list.get(lNode.level).add(lNode.node.val);
+            if (lNode.node.left != null)
+                queue.offer(new TreeNodeLevels(lNode.level + 1, lNode.node.left));
+            if (lNode.node.right != null)
+                queue.offer(new TreeNodeLevels(lNode.level + 1, lNode.node.right));
+        }
+        return list;
+    }
+}
